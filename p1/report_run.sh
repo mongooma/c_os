accb5a07c4a6b4146685a7193816eae4735d2390
mongooma <rhythm_qing@hotmail.com>
2019-03-25 15:36:49 -0400

    local at pycharms/cs_p1, 105/108 submitty

---
+++ p1/report_run.sh
@@ -0,0 +1,8 @@
+# https://submitty.cs.rpi.edu/index.php?semester=s19&course=csci4210&component=misc&page=display_file&dir=course_materials&file=p1.pdf&path=%2Fvar%2Flocal%2Fsubmitty%2Fcourses%2Fs19%2Fcsci4210%2Fuploads%2Fcourse_materials%2Fprojects%2Fp1%2Fp1.pdf
+
+# 1.Of the four simulated algorithms, which algorithm is the “best” algorithm? 
+# Which algorithm is best-suited for CPU-bound processes?  (long CPU bursts & short IO bursts)
+# Which algorithm is best-suited for I/O-bound processes?  (short CPU bursts & long IO bursts)
+# Support your answer by citing speciﬁc simulation results.
+
+# 
